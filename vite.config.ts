771ddb00321a6c12bbeb2e34eaa4767032d4b095
陈湘 <chenxiangbj@yuanfudao.com>
2023-12-18 14:32:05 +0800

    test pr

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
 import svgr from "vite-plugin-svgr";
-
+console.log('test')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),UnoCSS(),svgr()],
